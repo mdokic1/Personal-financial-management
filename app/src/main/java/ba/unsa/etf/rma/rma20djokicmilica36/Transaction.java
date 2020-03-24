ebfa179919222f2b074adcbf8c2be1ec2a4e51c0
mdokic1 <mdokic1@etf.unsa.ba>
2020-03-24 16:56:35 +0100

    Klasa Transaction i njeni atributi

---
+++ app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/Transaction.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rma.rma20djokicmilica36;
+
+public class Transaction {
+    private String date;
+    private int amount;
+    private String title;
+    private transactionType type;
+    private String itemDescription;
+    private Integer transactionInterval;
+    private String endDate;
+}
---
+++ app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/transactionType.java
@@ -0,0 +1,5 @@
+package ba.unsa.etf.rma.rma20djokicmilica36;
+
+public enum transactionType {
+    INDIVIDUALPAYMENT, REGULARPAYMENT,PURCHASE,INDIVIDUALINCOME,REGULARINCOME
+}
