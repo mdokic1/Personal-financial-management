c59fe0996e1040e6f0c4e303100239d768e52dd2
mdokic1 <mdokic1@etf.unsa.ba>
2020-03-25 23:19:55 +0100

    button.setOnClickListener za leftArrow i rightArrow

--- app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/MainActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/MainActivity.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rma.rma20djokicmilica36;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements ITransactionListV
     private ListView listView;
     private Button add;
 
+    private LocalDate trDatum;
+
     private TransactionListAdapter transactionListAdapter;
 
 
@@ -39,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements ITransactionListV
     }
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,11 +57,61 @@ public class MainActivity extends AppCompatActivity implements ITransactionListV
 
 
         LocalDate trenutniDatum = LocalDate.now();
+        trDatum = trenutniDatum;
         int mjesec = trenutniDatum.getMonthValue();
         int godina = trenutniDatum.getYear();
 
         month = (TextView) findViewById(R.id.month);
         month.setText(Months.values()[mjesec - 1] + "," + Integer.toString(godina));
+
+        leftArrow = (Button) findViewById(R.id.leftArrow);
+        rightArrow = (Button) findViewById(R.id.rightArrow);
+
+
+
+        leftArrow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int mj = trDatum.getMonthValue();
+                int god = trDatum.getYear();
+                if(mj >= 2){
+                    mj--;
+                    trDatum = trDatum.withMonth(mj);
+                    trDatum = trDatum.withYear(god);
+                }
+                else{
+                    mj = 12;
+                    god--;
+                    trDatum = trDatum.withMonth(mj);
+                    trDatum = trDatum.withYear(god);
+                }
+                month.setText(Months.values()[mj - 1] + "," + Integer.toString(god));
+                transactionListAdapter.notifyDataSetChanged();
+            }
+        });
+
+        rightArrow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int mj = trDatum.getMonthValue();
+                int god = trDatum.getYear();
+                if(mj <= 11){
+                    mj++;
+                    trDatum = trDatum.withMonth(mj);
+                    trDatum = trDatum.withYear(god);
+                }
+                else{
+                    mj = 1;
+                    god++;
+                    trDatum = trDatum.withMonth(mj);
+                    trDatum = trDatum.withYear(god);
+                }
+                month.setText(Months.values()[mj - 1] + "," + Integer.toString(god));
+                transactionListAdapter.notifyDataSetChanged();
+            }
+        });
 
     }
 
