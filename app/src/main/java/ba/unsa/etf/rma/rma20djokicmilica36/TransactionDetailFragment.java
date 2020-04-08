938e616bb6fb05dbb09ee7d325159670f8ae25cc
mdokic1 <mdokic1@etf.unsa.ba>
2020-04-08 23:59:10 +0200

    fragment_list.xml, fragment_detail.xml, activity_main.xml, klase TransactionListFragment i TransactionDetailFragment

---
+++ app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/TransactionDetailFragment.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rma.rma20djokicmilica36;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TransactionDetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Ovdje se dodjeljuje layout fragmentu,
+        // tj. ˇsta ´ce se nalaziti unutar fragmenta
+        //Ovu liniju ´cemo poslije promijeniti
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
+
+
+
+
