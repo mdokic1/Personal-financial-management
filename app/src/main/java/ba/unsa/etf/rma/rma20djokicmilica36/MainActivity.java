8deead13a1d2db961abf1d65c392331da58cbf8c
mdokic1 <mdokic1@etf.unsa.ba>
2020-05-25 02:48:34 +0200

    Azuriranje u twoPaneMode

--- app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/MainActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/rma20djokicmilica36/MainActivity.java
@@ -103,13 +103,76 @@ public class MainActivity extends AppCompatActivity implements TransactionListFr
     public void Refresh(){
         if(twoPaneMode){
             TransactionListFragment listFragment = (TransactionListFragment)getSupportFragmentManager().findFragmentById(R.id.transactions_list);
-            listFragment.getPresenter().refreshTransactions();
-            listFragment.getPresenter().refreshTransactionsByDate();
+            //listFragment.getPresenter().refreshTransactions();
+            //listFragment.getPresenter().refreshTransactionsByDate();
             Spinner filter = findViewById(R.id.filter);
             Spinner sort = findViewById(R.id.sort);
+            TextView month = findViewById(R.id.month);
             TextView glAmount = findViewById(R.id.glAmount);
             TextView lim = findViewById(R.id.lim);
-            listFragment.getPresenter().refreshTransactionsByTypeSorted(filter.getSelectedItem().toString(), sort.getSelectedItem().toString());
+            String typeId = "";
+            if(filter.getSelectedItem().toString().equals("Regular payment")){
+                typeId = "1";
+            }
+            if(filter.getSelectedItem().toString().equals("Regular income")){
+                typeId = "2";
+            }
+            if(filter.getSelectedItem().toString().equals("Purchase")){
+                typeId = "3";
+            }
+            if(filter.getSelectedItem().toString().equals("Individual income")){
+                typeId = "4";
+            }
+            if(filter.getSelectedItem().toString().equals("Individual payment")){
+                typeId = "5";
+            }
+            if(filter.getSelectedItem().toString().equals("All types") || filter.getSelectedItem().toString().equals("Filter by")){
+                typeId = "";
+            }
+
+            String sortTip = "";
+
+            if(sort.getSelectedItem().equals("Price - Ascending")){
+                sortTip = "amount.asc";
+            }
+            if(sort.getSelectedItem().equals("Price - Descending")){
+                sortTip = "amount.desc";
+            }
+            if(sort.getSelectedItem().equals("Title - Ascending")){
+                sortTip = "title.asc";
+            }
+            if(sort.getSelectedItem().equals("Title - Descending")){
+                sortTip = "title.desc";
+            }
+            if(sort.getSelectedItem().equals("Date - Ascending")){
+                sortTip = "date.asc";
+            }
+            if(sort.getSelectedItem().equals("Date - Descending")){
+                sortTip = "date.desc";
+            }
+            if(sort.getSelectedItem().equals("Sort by")){
+                sortTip = "";
+            }
+
+            String d = month.getText().toString();
+            int pozicija = d.indexOf(',');
+            String mj = d.substring(0, pozicija);
+            String god = d.substring(pozicija+1, d.length());
+
+            if(mj.equals("January")) mj = "01";
+            if(mj.equals("February")) mj = "02";
+            if(mj.equals("March")) mj = "03";
+            if(mj.equals("April")) mj = "04";
+            if(mj.equals("May")) mj = "05";
+            if(mj.equals("June")) mj = "06";
+            if(mj.equals("July")) mj = "07";
+            if(mj.equals("August")) mj = "08";
+            if(mj.equals("September")) mj = "09";
+            if(mj.equals("October")) mj = "10";
+            if(mj.equals("November")) mj = "11";
+            if(mj.equals("December")) mj = "12";
+            listFragment.getPresenter().refreshByDateTypeSorted(typeId, sortTip, mj, god);
+            //listFragment.getPresenter().refreshTransactionsByTypeSorted(filter.getSelectedItem().toString(), sort.getSelectedItem().toString());
             glAmount.setText("Global amount: " + round(listFragment.getPresenter().RefreshAmount(), 2));
             lim.setText("Limit: " + listFragment.getPresenter().RefreshLimit());
         }
